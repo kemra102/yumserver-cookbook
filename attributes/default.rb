c9a02fe73f3d7e44fe2873e448c04ef622655b65
Danny Roberts <danny@thefallenphoenix.net>
2015-10-11 18:01:50 +0100

    Removed un-needed attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,3 @@
 default['yumserver']['basepath'] = '/var/lib/yum-repo'
 default['yumserver']['nginx']['config_cookbook'] = 'yumserver'
 default['yumserver']['nginx']['servername'] = node['fqdn']
-
-default['nginx']['repo_source'] = 'nginx'
-default['nginx']['default_site_enabled'] = false
