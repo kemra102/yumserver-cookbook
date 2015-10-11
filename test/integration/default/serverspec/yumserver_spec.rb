e6697bb1d5699934418e5d09f0ac72e83679f548
Danny Roberts <danny@thefallenphoenix.net>
2015-10-11 14:18:47 +0100

    Added additional tests

--- test/integration/default/serverspec/yumserver_spec.rb
+++ test/integration/default/serverspec/yumserver_spec.rb
@@ -23,3 +23,23 @@ describe file('/var/lib/yum-repo/nginx') do
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
 end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe file('/etc/nginx/conf.d/yumserver') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'nginx' }
+  it { should be_grouped_into 'nginx' }
+end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
