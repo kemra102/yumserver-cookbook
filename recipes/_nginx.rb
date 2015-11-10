15d80183c7b08923782a1779e84d23c6f08fadca
Mark Bassett <mark.wil.bassett@hp.com>
2015-11-10 11:39:40 -0800

    fixes
    servername fix for _nginx template/recipe
    use node attributes instead of passing variable to template
    enable directory indexing
    append .conf to config file so that nginx reads it
    deactivate default nginx config

--- recipes/_nginx.rb
+++ recipes/_nginx.rb
@@ -7,19 +7,21 @@ end
 
 package 'nginx'
 
-template '/etc/nginx/conf.d/yumserver' do
+template '/etc/nginx/conf.d/yumserver.conf' do
   cookbook node['yumserver']['nginx']['config_cookbook']
   source 'nginx.erb'
   owner 'nginx'
   group 'nginx'
   mode '0644'
-  variables(
-    nginx_servername: 'servername'
-  )
+end
+
+execute 'remove_default_nginx' do
+  command 'mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.deactivated'
+  only_if { ::File.exists?('/etc/nginx/conf.d/default.conf') }
 end
 
 service 'nginx' do
   action [:enable, :start]
   supports restart: true, reload: true, status: true
-  subscribes :restart, 'template[/etc/nginx/conf.d/yumserver]', :delayed
+  subscribes :restart, 'template[/etc/nginx/conf.d/yumserver.conf]', :delayed
 end
