cb7d18cae2d48b07ad26dafc91023cdcef7f8d55
Danny Roberts <danny@thefallenphoenix.net>
2016-01-04 19:02:05 +0000

    Switch mirrored repo used for testing

--- test/fixtures/cookbooks/yumserver-test/recipes/default.rb
+++ test/fixtures/cookbooks/yumserver-test/recipes/default.rb
@@ -10,7 +10,7 @@ yumserver_mirror 'nginx' do
 end
 
 yumserver_rsync_mirror 'centos-xen' do
-  repo_name 'centos-kvm'
+  repo_name 'centos-xen'
   repo_description 'CentOS Virtualization Packages'
   repo_url "rsync://mirror.bytemark.co.uk/centos/#{major_ver[/./,0]}/virt/x86_64/xen/"
   action :create
