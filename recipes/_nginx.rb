d293fc8231b9995ec73730a10652016125e5f08c
Danny Roberts <danny@thefallenphoenix.net>
2016-02-04 23:14:09 +0000

    Fix hash syntax in nginx syntax

--- recipes/_nginx.rb
+++ recipes/_nginx.rb
@@ -1,7 +1,7 @@
 include_recipe 'nginx_server::default'
 
 nginx_server_vhost 'yumserver' do
-  listen [{'ipaddress': '0.0.0.0'}]
+  listen [{'ipaddress' => '0.0.0.0'}]
   server_name [node['hostname'], node['fqdn']]
   root node['yumserver']['basepath']
   config ({
